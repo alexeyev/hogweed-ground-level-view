5a95e546df788328a49192c27bda1c34ef144e76
alexeyev <anton.m.alexeyev@gmail.com>
2021-10-01 01:12:52 +0300

    [ENH] to device

--- prepared_pipeline_for_classification.py
+++ prepared_pipeline_for_classification.py
@@ -1,14 +1,12 @@
 # coding: utf-8
-import pandas as pd
 import torch
-import numpy as np
+from sklearn.model_selection import train_test_split
 from torch import nn
 from torch import optim
 from torch.nn.modules import loss
-from torch.utils.data import Subset, SubsetRandomSampler
+from torch.utils.data import Subset
 from torchvision import transforms
 
-# from sklearn.model_selection import train_test_split
 from dataset import HogweedClassificationDataset
 
 
@@ -24,6 +22,8 @@ def train(model, train_loader, optimizer, loss_function, current_epoch_number=0,
     loss_accum = 0
 
     for batch_idx, (data, target) in enumerate(train_loader):
+
+        print(batch_idx, ":", target)
 
         optimizer.zero_grad()
         output = model(data.to(device))
@@ -68,45 +68,57 @@ def test(model, test_loader, loss_function, device):
 if __name__ == "__main__":
 
     SEED = 100
-    SHORT_SIDE = 600
+    SHORT_SIDE = 30
 
     train_set = HogweedClassificationDataset(root="prepared_data/images_train",
                                              transform=transforms.Compose([
                                                  transforms.ToTensor(),
-                                                 # transforms.RandomRotation((270, 270)),
                                                  transforms.Resize(SHORT_SIDE)]))
 
-    print("Classes stats:", pd.Series(train_set.targets).value_counts())
+    # print("Classes stats:", pd.Series(train_set.targets).value_counts())
+    print(train_set.class_to_idx)
+    # print(train_set.extensions)
+    print(train_set.samples)
+    # print(len(train_set.samples))
+    # quit()
 
-    print("Saving transformed image sample...")
-    example(train_set, 10).save('out.jpg')
+    # print("Saving transformed image sample...")
+    # example(train_set, 10).save('out.jpg')
 
     print("Splitting data...")
-    print("Splitting data...")
 
-    # train_indices, dev_indices, _, _ = train_test_split(
-    #     range(len(train_set)), train_set.targets,
-    #     stratify=train_set.targets,
-    #     test_size=0.15,
-    #     shuffle=True,
-    #     random_state=SEED
-    # )
+    train_indices, val_indices, _, _ = train_test_split(
+        range(len(train_set)), train_set.targets,
+        stratify=train_set.targets,
+        test_size=0.15,
+        shuffle=True,
+        random_state=SEED
+    )
 
+    # print(pd.Series(np.array(train_set.targets)[train_indices]).value_counts())
+    # print(pd.Series(np.array(train_set.targets)[val_indices]).value_counts())
 
-    num_train = len(train_set)
-    indices = list(range(num_train))
-    split = int(0.15 * num_train)
+    # quit()
 
-    np.random.seed(SEED)
-    np.random.shuffle(indices)
+    # indices = list(range(len(train_set)))
+    # split = int(0.15 * len(train_set))
+    #
+    # np.random.seed(SEED)
+    # np.random.shuffle(indices)
+    #
+    # # todo: stratification?
+    # train_idx, val_idx = indices[split:], indices[:split]
+    #
+    # print(pd.Series(np.array(train_set.targets)[train_idx]).value_counts())
+    # print(pd.Series(np.array(train_set.targets)[val_idx]).value_counts())
 
-    # todo: stratification?
-    train_idx, val_idx = indices[split:], indices[:split]
-    train_sampler = SubsetRandomSampler(train_idx)
-    val_sampler = SubsetRandomSampler(val_idx)
+    # train_sampler = SubsetRandomSampler(train_idx)
+    # val_sampler = SubsetRandomSampler(val_idx)
+    # train_sampler = SubsetRandomSampler(train_indices)
+    # val_sampler = SubsetRandomSampler(val_indices)
 
-    train_loader = torch.utils.data.DataLoader(train_set, batch_size=32, sampler=train_sampler)
-    dev_loader = torch.utils.data.DataLoader(train_set, sampler=val_sampler)
+    train_loader = torch.utils.data.DataLoader(Subset(train_set, train_indices), batch_size=1, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(Subset(train_set, val_indices))
 
     print("CUDA available?", torch.cuda.is_available())
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -116,21 +128,20 @@ if __name__ == "__main__":
     # a dummy model that doesn't work; should be replaced
     # with a pretrained model for finetuning
     model = nn.Sequential(
-        nn.Conv2d(in_channels=3, out_channels=256, kernel_size=(3, 3), stride=(1, 1), padding=0),
+        nn.Conv2d(in_channels=3, out_channels=5, kernel_size=(3, 3), stride=(3, 3), padding=0),
         nn.ReLU(),
         nn.AdaptiveMaxPool2d(output_size=(1, 1)),
         nn.Flatten(),
-        nn.Linear(in_features=256, out_features=2),
-        nn.Softmax(dim=-1)
+        nn.Linear(in_features=5, out_features=2)
     ).to(device)
 
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.AdamW(model.parameters())
     loss_function = loss.CrossEntropyLoss()
 
     print("Starting training...")
 
     for epoch in range(1, 5):
         train(model, train_loader, optimizer, loss_function, epoch, device)
-        test(model, dev_loader, loss_function, device)
+        test(model, val_loader, loss_function, device)
 
     print("It is done.")
