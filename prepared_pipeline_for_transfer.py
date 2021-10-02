103ef34f0a924fac7d3d24dcbc967bc25bc40d25
alexeyev <anton.m.alexeyev@gmail.com>
2021-10-02 16:47:47 +0300

    [ENH] PlantCLEF

--- prepared_pipeline_for_transfer.py
+++ prepared_pipeline_for_transfer.py
@@ -11,12 +11,6 @@ from dataset import HogweedClassificationDataset
 from plant_clef_resnet import load_plant_clef_resnet18
 
 
-def example(dataset, i):
-    """ Preparing an image for viewing or saving """
-    print("oh", train_set[i][1])
-    return transforms.ToPILImage()(dataset[i][0])
-
-
 def train(model, train_loader, optimizer, loss_function, current_epoch_number=0, device=None):
     """ Training a provided model using provided data etc. """
     model.train()
@@ -26,7 +20,7 @@ def train(model, train_loader, optimizer, loss_function, current_epoch_number=0,
 
         optimizer.zero_grad()
         output = model(data.to(device))
-        loss = loss_function(output, target.to(device))
+        loss = loss_function(output, target.unsqueeze(dim=-1).float().to(device))
 
         # saving loss for stats
         loss_accum += loss.item() / len(data)
@@ -51,10 +45,10 @@ def test(model, test_loader, loss_function, device):
 
     with torch.no_grad():
         for data, target in test_loader:
-            target = target.to(device)
+            target = target.float().unsqueeze(dim=-1).to(device)
             output = model(data.to(device))
             test_loss += loss_function(output, target).sum().item()
-            pred = output.argmax(dim=1, keepdim=True)
+            pred = (torch.sign(output - 0.5) + 1) / 2
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
@@ -80,8 +74,8 @@ if __name__ == "__main__":
         random_state=SEED
     )
 
-    train_loader = torch.utils.data.DataLoader(Subset(train_set, train_indices), batch_size=32, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(Subset(train_set, val_indices))
+    train_loader = torch.utils.data.DataLoader(Subset(train_set, train_indices), batch_size=4, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(Subset(train_set, val_indices), shuffle=False, batch_size=128)
 
     print("CUDA available?", torch.cuda.is_available())
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -91,19 +85,18 @@ if __name__ == "__main__":
 
     for ch_id, child in enumerate(pretrained_resnet.children()):
         for _, param in enumerate(child.parameters()):
-            if param.requires_grad is not None:
-                param.requires_grad = False
+            param.requires_grad = False
 
     pretrained_resnet.fc = nn.Sequential(
         nn.Linear(in_features=512, out_features=64),
         nn.ReLU(),
-        nn.Linear(in_features=64, out_features=2)
+        nn.Linear(in_features=64, out_features=1)
     )
 
     pretrained_resnet = pretrained_resnet.to(device)
 
-    optimizer = optim.AdamW(pretrained_resnet.parameters(), amsgrad=True)
-    loss_function = loss.CrossEntropyLoss()
+    optimizer = optim.Adam(pretrained_resnet.parameters())
+    loss_function = loss.BCEWithLogitsLoss()
 
     print("Starting training...")
 
